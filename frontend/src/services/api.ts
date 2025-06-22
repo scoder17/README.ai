87a16ee9782671704cd8822c92c8fe5ede82894c
Adarsh <starcoder17@gmail.com>
2025-06-23 00:22:56 +0530

    modified api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const generateReadme = async (repoUrl: string) => {
-  const response = await axios.post("http://localhost:8000/api/generate-readme/", {
+  const response = await axios.post("${process.env.REACT_APP_API_URL}/api/generate-readme/", {
     repo_url: repoUrl,
   });
   return response.data;
