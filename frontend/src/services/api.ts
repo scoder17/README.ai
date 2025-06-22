82e6138f42e94a616a60afddd06611dd2fd30f4a
Adarsh <starcoder17@gmail.com>
2025-06-23 01:28:33 +0530

    fixed bug in api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const generateReadme = async (repoUrl: string) => {
-  const response = await axios.post("${process.env.REACT_APP_API_URL}/api/generate-readme/", {
+  const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/generate-readme/`, {
     repo_url: repoUrl,
   });
   return response.data;
